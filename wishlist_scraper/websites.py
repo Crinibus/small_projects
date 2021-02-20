a3247f427d3904f6fcfb8d5225adca82c3457bdf
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-02-21 00:29:38 +0100

    Format with Black
    const.py and websites.py

--- wishlist_scraper/websites.py
+++ wishlist_scraper/websites.py
@@ -8,63 +8,77 @@ class Websites:
     def Komplett(link: str):
         # get name and price of product at link
         response = request_link(link)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('div', class_='product-main-info__info').h1.span.text.lower()
-        price = soup.find('span', class_='product-price-now').text.strip(',-').replace('.', '')
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("div", class_="product-main-info__info").h1.span.text.lower()
+        price = (
+            soup.find("span", class_="product-price-now")
+            .text.strip(",-")
+            .replace(".", "")
+        )
         return name, price
 
     @staticmethod
     def Proshop(link: str):
         # get name and price of product at link
         response = request_link(link)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('div', class_='col-xs-12 col-sm-7').h1.text.lower()
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("div", class_="col-xs-12 col-sm-7").h1.text.lower()
 
         try:
             # find normal price
-            price = soup.find('span', class_='site-currency-attention').text.split(',')[0].replace('.', '')
+            price = (
+                soup.find("span", class_="site-currency-attention")
+                .text.split(",")[0]
+                .replace(".", "")
+            )
         except AttributeError:
             try:
                 # find discount price
-                price = soup.find('div', class_='site-currency-attention site-currency-campaign').text.split(',')[0].replace('.', '')
+                price = (
+                    soup.find(
+                        "div", class_="site-currency-attention site-currency-campaign"
+                    )
+                    .text.split(",")[0]
+                    .replace(".", "")
+                )
             except AttributeError:
                 # if campaign is sold out (udsolgt)
-                price = soup.find('div', class_='site-currency-attention').text.split(',')[0].replace('.', '')
+                price = (
+                    soup.find("div", class_="site-currency-attention")
+                    .text.split(",")[0]
+                    .replace(".", "")
+                )
         return name, price
 
     @staticmethod
     def AvXperten(link: str):
         # get name and price of product at link
         response = request_link(link)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        name = soup.find('div', class_='content-head').text.strip().lower()
-        price = soup.find('div', class_='price').text.replace(u'\xa0DKK', '')
+        soup = BeautifulSoup(response.text, "html.parser")
+        name = soup.find("div", class_="content-head").text.strip().lower()
+        price = soup.find("div", class_="price").text.replace(u"\xa0DKK", "")
         return name, price
 
 
 GET_WEBSITE_METHOD = {
     "komplett": Websites.Komplett,
     "proshop": Websites.Proshop,
-    "avxperten": Websites.AvXperten
+    "avxperten": Websites.AvXperten,
 }
 
 
 def request_link(link: str):
-    response = requests.get(
-        link,
-        headers=REQUEST_HEADER,
-        cookies=REQUEST_COOKIES
-    )
+    response = requests.get(link, headers=REQUEST_HEADER, cookies=REQUEST_COOKIES)
 
     return response
 
 
 def get_domain_from_link(link: str):
     # get domain with "www." and ".com"
-    domain = link.split('/')[2]
+    domain = link.split("/")[2]
 
     # remove "www." and ".com"
     for to_remove in REMOVE_FROM_DOMAIN:
-        domain = domain.replace(to_remove, '')
-    
+        domain = domain.replace(to_remove, "")
+
     return domain
