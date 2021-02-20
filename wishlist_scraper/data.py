601afb2aaa64c3e9ec56e0c922eb39803a1101aa
Crinibus <57172157+Crinibus@users.noreply.github.com>
2021-02-21 00:22:05 +0100

    Update data.py
    Formatted with Black

--- wishlist_scraper/data.py
+++ wishlist_scraper/data.py
@@ -12,17 +12,17 @@ class Data:
 
     @staticmethod
     def read_json(filename: str) -> dict:
-        with open(filename, 'r') as json_file:
+        with open(filename, "r") as json_file:
             data = json.load(json_file)
         return data
-    
+
     def format_data(self):
         # for category in self.data.keys():
         #     self.categories.update({category: []})
-        
+
         #     for sub_category in self.data[category]:
         #         self.categories[category].append(SubCategory(sub_category, self.data[category][sub_category]))
-        
+
         for category in self.data.keys():
             self.add_category(category, self.data[category])
 
@@ -37,11 +37,11 @@ class SuperCategory:
         self.info = info
         self.sub_categories = []
         self.format_info()
-    
+
     def format_info(self):
         for sub_cat in self.info:
             self.add_sub_category(sub_cat, self.info[sub_cat])
-    
+
     def add_sub_category(self, name: str, info: dict):
         new_sub_category = SubCategory(name, info)
         self.sub_categories.append(new_sub_category)
@@ -61,7 +61,7 @@ class SubCategory:
     def add_product(self, link: str):
         new_product = Product(link)
         self.products.append(new_product)
-    
+
     def get_info_for_products(self):
         # Create threads
         threads = [
@@ -92,7 +92,7 @@ class Product:
         self.domain = get_domain_from_link(link)
         self.name = None
         self.price = None
-        #self.get_info()
+        # self.get_info()
 
     def get_info(self):
         self.name, self.price = GET_WEBSITE_METHOD[self.domain](self.link)
